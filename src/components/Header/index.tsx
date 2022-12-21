d6fb5f0dfd1b1e84ccd82dc336f7b4d7fc55b75b
yurimarcolino <yurifmarcolino@gmail.com>
2022-12-21 10:42:20 -0300

    feat: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,29 @@
+import { NavLink } from 'react-router-dom'
+import {
+  HeaderContainerStyled,
+  LocationContainerStyled,
+  NavContainerStyled,
+} from './styles'
+
+import logoSVG from '../../assets/logo.svg'
+import locationSVG from '../../assets/location.svg'
+import cartSVG from '../../assets/cart.svg'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainerStyled>
+      <img src={logoSVG} alt="coffee icon" />
+      <NavContainerStyled>
+        <LocationContainerStyled>
+          <img src={locationSVG} alt="location icon" />
+          <span>Porto Alegre, RS</span>
+        </LocationContainerStyled>
+        <nav>
+          <NavLink to="/checkout" title="Checkout">
+            <img src={cartSVG} alt="cart icon" />
+          </NavLink>
+        </nav>
+      </NavContainerStyled>
+    </HeaderContainerStyled>
+  )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,46 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainerStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0rem;
+`
+
+export const NavContainerStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.75rem;
+
+  nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 2.375rem;
+    height: 2.375rem;
+    border-radius: 0.375rem;
+
+    background-color: ${(props) => props.theme['yellow-100']};
+  }
+`
+
+export const LocationContainerStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  gap: 0.25rem;
+
+  width: 8.9375rem;
+  height: 2.375rem;
+
+  background-color: ${(props) => props.theme['purple-100']};
+  border-radius: 0.375rem;
+
+  span {
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['purple-500']};
+  }
+`
