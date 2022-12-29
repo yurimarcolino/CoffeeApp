3af335896c963723f6175b4b23865ed3115bfd69
yurimarcolino <yurifmarcolino@gmail.com>
2022-12-29 13:10:54 -0300

    refactor: importing context functions

--- src/components/CoffeeSelected/index.tsx
+++ src/components/CoffeeSelected/index.tsx
@@ -1,35 +1,64 @@
 import { Minus, Plus, Trash } from 'phosphor-react'
+import { useCartContext } from '../../context/CartContext'
 import { CoffeeSelectedContainerStyled, DividerStyled } from './styled'
 
-export function CoffeeSelected() {
+interface Coffee {
+  id: string
+  description: string
+  imagePath: string
+  name: string
+  price: string
+  quantity: number
+}
+
+interface CoffeeSelectedProps {
+  coffee: Coffee
+}
+
+export function CoffeeSelected({ coffee }: CoffeeSelectedProps) {
+  const {
+    decreaseCoffeeQuantityFromCart,
+    increaseCoffeeQuantityFromCart,
+    removeCoffeeFromCart,
+  } = useCartContext()
+
+  const total = parseFloat(coffee.price) * coffee.quantity
+
   return (
     <CoffeeSelectedContainerStyled>
       <div className="CoffeeSelectedContent">
         <div className="CoffeeSelectedInfo">
-          <img
-            src="src/assets/expresso-tradicional.svg"
-            alt="Coffee selected image"
-          />
+          <img src={coffee.imagePath} alt="Coffee selected image" />
           <div className="CoffeeSelectedDetails">
-            <p>Expresso Tradicional</p>
+            <p>{coffee.name}</p>
             <div className="CartOptions">
               <div>
-                <button>
+                <button
+                  disabled={coffee.quantity === 1}
+                  onClick={() => decreaseCoffeeQuantityFromCart(coffee.id)}
+                >
                   <Minus weight="bold" size={16} />
                 </button>
-                <span className="CoffeeSelectedQuantity">1</span>
-                <button>
+                <span className="CoffeeSelectedQuantity">
+                  {coffee.quantity}
+                </span>
+                <button
+                  onClick={() => increaseCoffeeQuantityFromCart(coffee.id)}
+                >
                   <Plus weight="bold" size={15} />
                 </button>
               </div>
-              <button className="RemoveIconButtonContainer">
+              <button
+                className="RemoveIconButtonContainer"
+                onClick={() => removeCoffeeFromCart(coffee.id)}
+              >
                 <Trash weight="bold" size={16} />
                 Remover
               </button>
             </div>
           </div>
         </div>
-        <span className="CoffeeSelectedPrice">R$ 9,90</span>
+        <span className="CoffeeSelectedPrice">R$ {total.toFixed(2)}</span>
       </div>
       <DividerStyled />
     </CoffeeSelectedContainerStyled>
