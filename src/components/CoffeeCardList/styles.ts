44ac88b0559c3ab6f994e120b37639d0bda026d4
yurimarcolino <yurifmarcolino@gmail.com>
2022-12-29 13:09:41 -0300

    refactor: centering the list

--- src/components/CoffeeCardList/styles.ts
+++ src/components/CoffeeCardList/styles.ts
@@ -2,7 +2,11 @@ import styled from 'styled-components'
 
 export const CoffeeCardListStyled = styled.div`
   display: flex;
+  justify-content: flex-start;
   flex-wrap: wrap;
-  padding-bottom: 9.8125rem;
+
+  margin-bottom: 9.8125rem;
   gap: 2.5rem 2rem;
+
+  max-width: 1120px;
 `
