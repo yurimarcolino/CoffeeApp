df4dab1dd92329c07ccea4a3071d3aa11f5fe3da
yurimarcolino <yurifmarcolino@gmail.com>
2022-12-29 13:13:33 -0300

    feat: adding cart value function

--- src/pages/Checkout/components/CoffeeOrder/TotalPrice/index.tsx
+++ src/pages/Checkout/components/CoffeeOrder/TotalPrice/index.tsx
@@ -1,19 +1,39 @@
+import { Coffee } from '../../../../../context/CartContext'
 import { TotalPriceContainerStyled } from './styles'
 
-export function TotalPrice() {
+interface TotalPriceProps {
+  cart: Coffee[]
+}
+
+const DELIVERY_VALUE: number = 3.5
+
+export function TotalPrice({ cart }: TotalPriceProps) {
+  const cartValue = cart.reduce(
+    (acc, item) => acc + parseFloat(item.price) * item.quantity,
+    0,
+  )
+
+  let totalValue
+
+  if (cart.length) {
+    totalValue = cartValue + DELIVERY_VALUE
+  } else {
+    totalValue = cartValue
+  }
+
   return (
     <TotalPriceContainerStyled>
       <div>
         <span>Total de itens</span>
-        <span>R$ 29,70</span>
+        <span>R$ {cartValue.toFixed(2)}</span>
       </div>
       <div>
         <span>Entrega</span>
-        <span>R$ 3,50</span>
+        <span>R$ {cart.length > 0 ? DELIVERY_VALUE : 0}</span>
       </div>
       <div>
         <span id="total-text">Total</span>
-        <span id="total-price">R$ 33,20</span>
+        <span id="total-price">R$ {totalValue.toFixed(2)}</span>
       </div>
     </TotalPriceContainerStyled>
   )
