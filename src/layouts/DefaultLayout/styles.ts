544a71db0c1ec10fab9ba2748c0c3b067764cdde
yurimarcolino <yurifmarcolino@gmail.com>
2022-12-29 13:12:35 -0300

    refactor: updating screen behavior

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,14 +1,15 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  margin: 0 35rem;
+  margin: 0 auto;
   padding: 0 10rem;
 
-  width: 1440px;
-  height: 100%;
+  width: 100vh;
+  height: 100vh;
 
   background: ${(props) => props.theme['gray-100']};
 
   display: flex;
   flex-direction: column;
+  align-items: center;
 `
