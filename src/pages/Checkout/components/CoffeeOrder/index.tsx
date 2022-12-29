0b4b00e00938022f770e4e07f62faaaa4504f1f2
yurimarcolino <yurifmarcolino@gmail.com>
2022-12-29 13:13:56 -0300

    feat: rendering the selected coffee list

--- src/pages/Checkout/components/CoffeeOrder/index.tsx
+++ src/pages/Checkout/components/CoffeeOrder/index.tsx
@@ -1,14 +1,18 @@
 import { Button } from '../../../../components/Button'
 import { CoffeeSelected } from '../../../../components/CoffeeSelected'
+import { useCartContext } from '../../../../context/CartContext'
 import { CoffeeOrderContainerStyled } from './styles'
 import { TotalPrice } from './TotalPrice'
 
 export function CoffeeOrder() {
+  const { cart } = useCartContext()
+
   return (
     <CoffeeOrderContainerStyled>
-      <CoffeeSelected />
-      <CoffeeSelected />
-      <TotalPrice />
+      {cart.map((coffee) => {
+        return <CoffeeSelected key={coffee.id} coffee={coffee} />
+      })}
+      <TotalPrice cart={cart} />
       <Button />
     </CoffeeOrderContainerStyled>
   )
