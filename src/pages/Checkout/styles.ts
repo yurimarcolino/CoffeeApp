cb4d512c88c0fe7f0ba774c8c1f7daa58f668288
yurimarcolino <yurifmarcolino@gmail.com>
2023-01-02 12:08:09 -0300

    refactor: changing wrapper to be a form

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const CheckoutContainerStyled = styled.div`
+export const CheckoutContainerStyled = styled.form`
   display: flex;
   flex-direction: row;
   gap: 2rem;
@@ -28,5 +28,30 @@ export const CheckoutContainerStyled = styled.div`
       color: ${(props) => props.theme['brown-900']};
       margin-bottom: 0.9375rem;
     }
+  }
+`
+
+export const ErrorStyled = styled.p`
+  position: absolute;
+  bottom: 41.5px;
+  color: red;
+  font-size: 0.65rem;
+`
+
+export const InputContainerStyled = styled.div`
+  position: relative;
+  margin-bottom: 1rem;
+
+  input {
+    display: flex;
+    align-items: center;
+    padding: 0.75rem;
+
+    height: 2.625rem;
+
+    background: ${(props) => props.theme['gray-300']};
+
+    border: 0.0625rem solid ${(props) => props.theme['gray-400']};
+    border-radius: 0.25rem;
   }
 `
